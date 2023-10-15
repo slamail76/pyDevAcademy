d375b81beaedde8155d7af9d87e75dca27dbc12c
stefano <slamail76@gmail.com>
2023-10-15 15:18:00 +0200

    good version

---
+++ Utility/youtube/playlist2.py
@@ -0,0 +1,28 @@
+from pytube import Playlist
+from pytube import YouTube as YT
+import time
+
+playlist = Playlist('https://www.youtube.com/playlist?list=PLJ1jW-5wZ--YMUqj4qUv1W03OTHu7TY4c')
+n = 1
+for video in playlist.videos:
+    playlist = Playlist('https://www.youtube.com/playlist?list=PLJ1jW-5wZ--YMUqj4qUv1W03OTHu7TY4c')
+    while True:
+        try:
+            video.title = playlist.title
+            break
+        except:
+            print("Failed to get name. Retrying...")
+            time.sleep(1)
+            playlist = Playlist('https://www.youtube.com/playlist?list=PLJ1jW-5wZ--YMUqj4qUv1W03OTHu7TY4c')
+            continue
+    video.title = (f'{n} {video.title}')
+    print(video.title)
+    #video = YT(playlist.videos[i], use_oauth=True, allow_oauth_cache=True)
+    #stream = video.streams.get_by_itag(140)
+    st = video.streams.get_highest_resolution()
+    st.download(output_path='D:\Download\Basedati')
+    if ((n % 2) == 0):
+        time.sleep(5)
+    if ((n % 5) == 0):
+        time.sleep(15)
+    n += 1
